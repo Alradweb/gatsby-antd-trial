1067de2193b204d0bcad0720d049f7664688e925
Alradweb <alexradin1964@gmail.com>
2019-07-11 18:34:15 +0300

    Update config-gatsby.js and .gitignore file

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,11 +22,10 @@ module.exports = {
     {
       resolve: 'gatsby-source-strapi',
       options: {
-        apiURL: 'http://localhost:1337',
-        contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
-          'article',
-          'user'
-        ],
+        apiURL: process.env.DEPLOY_URL
+          ? "https://intense-inlet-52562.herokuapp.com"
+          : "http://localhost:1337",
+        contentTypes: [`article`, `user`],
         queryLimit: 1000,
       },
     },
