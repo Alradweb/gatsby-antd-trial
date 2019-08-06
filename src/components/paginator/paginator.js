45d4f3584a7ffdc949f996431a50ec5be03bd4fd
Alradweb <alexradin1964@gmail.com>
2019-08-06 21:11:34 +0300

    test pagination

--- src/components/paginator/paginator.js
+++ src/components/paginator/paginator.js
@@ -1,44 +1,46 @@
-import React from 'react'
+import React from "react"
 import { Link } from "gatsby"
-import styles from './app-paginator.module.css'
+import styles from "./app-paginator.module.css"
+import { Icon } from "antd"
 
 
-const Paginator = (props) =>{
+const Paginator = (props) => {
   console.log(props)
   const { currentPage, numPages, pageName } = props
   const isFirst = currentPage === 1
   const isLast = currentPage === numPages
   const prevPage = currentPage - 1 === 1 ? "/" : (currentPage - 1).toString()
   const nextPage = (currentPage + 1).toString()
-  return(
+  return (
     <section className={styles.paginateWrapper}>
       {!isLast && (
         <Link to={`/${pageName}/${nextPage}`} className={styles.button} rel="next">
-          Следующая страница »
+          Следующая страница <Icon type="double-right" style={{color: '#ffffff'}}/>
         </Link>
       )}
       <ul className={styles.pagination}>
         <li>
           {!isFirst && (
             <Link to={`/${pageName}/${prevPage}`} rel="prev">
-              ← Предыдущая страница
+              <Icon type="double-left" style={{color: 'tomato', fontSize: '15px'}} />
             </Link>
           )}
         </li>
         {Array.from({ length: numPages }, (_, i) => (
-          <li key={`pagination-number${i + 1}`} >
-            <Link to={`/${pageName}/${i === 0 ? "" : i + 1}`}>
+          <li key={`pagination-number-${i + 1}`}>
+            <Link to={`/${pageName}/${i === 0 ? "" : i + 1}`} className={styles.number}>
               {i + 1}
             </Link>
           </li>
         ))}
-        <li>
-          {!isLast && (
+        {!isLast && (
+          <li>
             <Link to={`/${pageName}/${nextPage}`} rel="next">
-              Следующая страница →
+              <Icon type="double-right" style={{color: 'tomato', fontSize: '15px'}}/>
             </Link>
-          )}
-        </li>
+          </li>
+        )}
+
       </ul>
 
     </section>
